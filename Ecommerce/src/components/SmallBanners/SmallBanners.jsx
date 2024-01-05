b498e7282e0e65fd25c8a410615f817f016504ae
notebook samsung dev-celo <marcelohenrique.backend@gmail.com>
2024-01-05 11:31:28 -0300

    Feat: SmallBanners implemented

---
+++ Ecommerce/src/components/SmallBanners/SmallBanners.jsx
@@ -0,0 +1,20 @@
+import './smallBaners.css';
+
+export function SmallBaners() {
+  return (
+    <section id="banner3">
+      <div className="banner-box">
+        <h2>Season Sale</h2>
+        <h3>Winter collection - 50% off</h3>
+      </div>
+      <div className="banner-box banner-box2">
+        <h2>New Footwear</h2>
+        <h3>Footwear collection - 40% off</h3>
+      </div>
+      <div className="banner-box banner-box3">
+        <h2>T-shirts</h2>
+        <h3>Shirts collection - 30% off</h3>
+      </div>
+    </section>
+  );
+}
