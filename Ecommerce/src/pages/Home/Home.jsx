8667c05f1032d12be1a1ecc057c5c724f30dc5de
notebook samsung dev-celo <marcelohenrique.backend@gmail.com>
2024-01-05 11:32:17 -0300

    Feat: Home complemented

--- Ecommerce/src/pages/Home/Home.jsx
+++ Ecommerce/src/pages/Home/Home.jsx
@@ -1,6 +1,12 @@
+import { Banner } from "../../components/BannerDiscount/Banner";
+import { DoubleBanner } from "../../components/DoubleBanner/DoubleBanner";
 import { Feature } from "../../components/Feature/Feature";
+import { FirstProduct } from "../../components/FirstProduct/FirstProduct";
+import { Footer } from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header"
 import { Hero } from "../../components/Hero/Hero";
+import { SecondProduct } from "../../components/SecondProduct/SecondProduct";
+import { SmallBaners } from "../../components/SmallBanners/SmallBanners";
 
 function Home() {
   return (
@@ -8,6 +14,12 @@ function Home() {
       <Header />
       <Hero />
       <Feature />
+      <FirstProduct />
+      <Banner />
+      <SecondProduct />
+      <DoubleBanner />
+      <SmallBaners />
+      <Footer />
     </>
   )
 }
