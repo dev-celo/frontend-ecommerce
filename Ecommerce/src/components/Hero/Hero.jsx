f047be2bd1fa0aa79e440c3c8ed53ed112ae0a36
dev-celo <marcelohenrique.backend@gmail.com>
2024-01-29 17:17:19 -0300

    Alteração realizada com segurança diretamente na branch main; todo site ainda passará por testes

---
+++ Ecommerce/src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import './hero.css';
+
+function Hero() {
+    return (
+      <section id="hero" className="cont">
+        <h4>Trade-in-offer</h4>
+        <h2>Super value deals</h2>
+        <h1>On all products</h1>
+        <p>Save more with coupons & up to 70% off!</p>
+        <button>Shop Now</button>
+      </section>
+    );
+}
+
+export default Hero
