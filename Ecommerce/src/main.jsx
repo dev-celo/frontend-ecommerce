59e5dcb2ea4560d65f2a0050e9d1a709a9ceb89f
dev-celo <marcelohenrique.backend@gmail.com>
2024-01-30 11:29:18 -0300

    Feat: Implementando rotas e context

--- Ecommerce/src/main.jsx
+++ Ecommerce/src/main.jsx
@@ -1,8 +1,13 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// Main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <App />
-  </Router>,
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
