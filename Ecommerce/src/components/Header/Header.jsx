ab6126f5ab19615220c3dfc349c542c9a75f6d47
dev-celo <marcelohenrique.backend@gmail.com>
2024-02-08 19:44:27 -0300

    Fix: Refactoring the responsive menu of navbar

--- Ecommerce/src/components/Header/Header.jsx
+++ Ecommerce/src/components/Header/Header.jsx
@@ -1,36 +1,75 @@
+import { useState, useEffect } from 'react';
 import './header.css';
 import logo from '../../img/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const close = document.getElementById('close');
+    let bar = document.getElementById('bar');
+
+
+    const openBar = () => {
+      setMobileMenuOpen(true);
+      bar.style.display = 'none';
+      close.style.display = 'flex';
+    };
+
+    const closeBar = () => {
+      setMobileMenuOpen(false);
+      close.style.display = 'none';
+      bar.style.display = 'flex';
+    };
+
+    if (close) {
+      close.addEventListener('click', closeBar);
+    }
+
+    if (bar) {
+      bar.addEventListener('click', openBar);
+    }
+
+    return () => {
+      if (close) {
+        close.removeEventListener('click', closeBar);
+      }
+
+      if (bar) {
+        bar.removeEventListener('click', openBar);
+      }
+    };
+  }, []);
+
   return (
     <>
       <section id="header">
-        <a href="#"><img src={logo} className="logo" alt="cura logo da páginax" /></a>
+        <a href="#">
+          <img src={logo} className="logo" alt="cura logo da página" />
+        </a>
 
         <div>
-          <ul id="navbar">
+          <ul id="navbar" className={isMobileMenuOpen ? 'active' : ''}>
             <li><a className="active" href="index.html">Home</a></li>
             <li><a href="shop.html">Shop</a></li>
             <li><a href="blog.html">Blog</a></li>
             <li><a href="about.html">About</a></li>
             <li><a href="contact.html">Contact</a></li>
             <li>
-              <a id="lg-bab" href="cart.html"><i className="fas fa-shopping-bag"></i></a>
+              <a id="lg-bab" href="cart.html">Carrinho</a>
             </li>
-
-            <a href="#" id="close"><i className="fas fa-times"></i></a>
           </ul>
         </div>
 
-        <div id="mobile">        
-          <a href="cart.html"><i className="fas fa-shopping-bag"></i></a>
-
-          <i onClick="openBar()" id="bar" className="fas fa-outdent"></i>
+        <div id="mobile">
+          <FontAwesomeIcon icon="shopping-cart" />
+          <FontAwesomeIcon id="bar" icon="bars" />
+          <FontAwesomeIcon id="close" style={{ display: 'none' }} icon="times" />
         </div>
-
       </section>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- Ecommerce/src/main.jsx
+++ Ecommerce/src/main.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faTimes, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars, faTimes, faShoppingCart);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
