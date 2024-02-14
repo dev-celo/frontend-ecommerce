675c84c5c65642460d7e2620cb70f534a3619d74
dev-celo <marcelohenrique.backend@gmail.com>
2024-02-13 23:17:27 -0300

    Feat: Implementando pageLogin

---
+++ Ecommerce/src/pages/Login/PageLogin.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import Login from "../../components/Login/Login";
+
+function PageLogin() {
+    return (
+        <>
+            <Header />
+            <Login />
+    </>
+    )
+}
+
+export default PageLogin;
