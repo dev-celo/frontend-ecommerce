888e83ec33a5153ae84fcb7455b69e54c0b26a8f
dev-celo <marcelohenrique.backend@gmail.com>
2024-02-13 23:18:38 -0300

    Fix: Adaptando rotas

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -3,12 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import DetailsProduct from './pages/DetailsProduct/DetailsProduct';
 import './app.css';
+import PageLogin from './pages/Login/PageLogin';
+import PageRegister from './pages/PageRegister/PageRegister';
 
 function App() {
   return (
     <AppProvider>
       <Routes>
         <Route path="/" Component={ Home } />
+        <Route path="/login" Component={ PageLogin } />
+        <Route path="/register" Component={ PageRegister } />
         <Route path="/product/:typeProduct/:id" Component={ DetailsProduct } />
       </Routes>
     </AppProvider>
