6120598867cc639bbebeb5e8130d909b86fa0816
dev-celo <marcelohenrique.backend@gmail.com>
2024-03-14 09:58:06 -0300

    Feat: initial commit RTL

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    css: true,
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      provider: 'v8'
+    }
+  },
   base: "/ecommerce/",
   server: {
     open: true,
@@ -14,5 +27,5 @@ export default defineConfig({
         manualChunks: undefined, // Isso desativa a criação automática de chunks
       },
     }
-  }    
+  }
 })
