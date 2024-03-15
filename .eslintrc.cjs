cdf27242dc60e9ddbf05d90114d006cd201effc7
dev-celo <marcelohenrique.backend@gmail.com>
2024-03-15 17:27:42 -0300

    feat: Configurando ambiente de testes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,18 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
+  globals: {
+    "test": true,
+    "describe": true,
+    "expect": true,
+    "jest": true,
+    "beforeEach": true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
