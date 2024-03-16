9a1277ec4e6d87022b7261e6255e8381130ebe18
dev-celo <marcelohenrique.backend@gmail.com>
2024-03-16 11:07:46 -0300

    Fix: Incrementando ids e data-testids

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,15 @@
 import './hero.css';
 
 function Hero() {
-    return (
-      <section id="hero" className="cont">
-        <h4>Trade-in-offer</h4>
-        <h2>Super value deals</h2>
-        <h1>On all products</h1>
-        <p>Save more with coupons & up to 70% off!</p>
-        <button>Shop Now</button>
-      </section>
-    );
+  return (
+    <section id="hero" data-testid="hero" className="cont">
+      <h4 id="trade-offer" data-testid="trade-offer">Trade-in-offer</h4>
+      <h2 id="super-deals" data-testid="super-deals">Super value deals</h2>
+      <h1 id="all-products" data-testid="all-products">On all products</h1>
+      <p id="coupons" data-testid="coupons">Save more with coupons & up to 70% off!</p>
+      <button id="shop-now" data-testid="shop-now">Shop Now</button>
+    </section>
+  );
 }
 
 export default Hero
