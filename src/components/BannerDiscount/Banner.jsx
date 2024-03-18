29deac23afdccd21aeb328c7373b3974f7605896
dev-celo <marcelohenrique.backend@gmail.com>
2024-03-18 10:30:38 -0300

    Feat: Testes de banners

--- src/components/BannerDiscount/Banner.jsx
+++ src/components/BannerDiscount/Banner.jsx
@@ -2,10 +2,10 @@ import './banner.css';
 
 export function Banner() {
   return (
-    <section id="banner" className="section-m1">
-      <h4>Repair Service</h4>
-      <h2>Up to <span>70% off</span> - All t-Shirt & Accessories</h2>
-      <button className="normal">Explore More</button>
+    <section id="banner" className="section-m1" data-testid="banner">
+      <h4 data-testid="banner-title">Repair Service</h4>
+      <h2 data-testid="banner-subtitle">Up to <span>70% off</span> - All t-Shirt & Accessories</h2>
+      <button className="normal" data-testid="banner-button">Explore More</button>
     </section>
   );
 }
--- src/components/DoubleBanner/DoubleBanner.jsx
+++ src/components/DoubleBanner/DoubleBanner.jsx
@@ -2,18 +2,18 @@ import './doubleBanner.css';
 
 export function DoubleBanner() {
   return (
-    <section id="sm-banner" className="section-p1">
-      <div className="banner-box">
-        <h4>Craze deals</h4>
-        <h2>Buy 1 get 1 free</h2>
-        <span>The best classNameic dress in on sale at cara</span>
-        <button className="white">Learn More</button>
+    <section id="double-banner" className="section-p1" data-testid="double-banner">
+      <div className="banner-box" data-testid="banner-box-1" id="banner-box-1">
+        <h4 data-testid="banner-title-1">Craze deals</h4>
+        <h2 data-testid="banner-subtitle-1">Buy 1 get 1 free</h2>
+        <span data-testid="banner-description-1">The best classNameic dress in on sale at cara</span>
+        <button className="white" data-testid="banner-button-1">Learn More</button>
       </div>
-      <div className="banner-box banner-box2">
-        <h4>spring/summer</h4>
-        <h2>Upcomming season</h2>
-        <span>The best classNameic dress in on sale at cara</span>
-        <button className="white">Collection</button>
+      <div className="banner-box banner-box2" data-testid="banner-box-2" id="banner-box-2">
+        <h4 data-testid="banner-title-2">spring/summer</h4>
+        <h2 data-testid="banner-subtitle-2">Upcomming season</h2>
+        <span data-testid="banner-description-2">The best classNameic dress in on sale at cara</span>
+        <button className="white" data-testid="banner-button-2">Collection</button>
       </div>
     </section>
   );
