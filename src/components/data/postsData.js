9fd671f6edc76181d250281e2c2cd9e9a7305bd8
dev-celo <marcelohenrique.backend@gmail.com>
2024-04-10 13:03:37 -0300

    Fix: Adicionando um header e modificando caminho para imagens

--- src/components/data/postsData.js
+++ src/components/data/postsData.js
@@ -2,31 +2,31 @@ export const postsData = [
     {
         "title": "The cotton-jersey",
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
-        "image": "img/blog/b1.jpg",
+        "image": "/ecommerce/img/blog/fashion.png",
         "date": "13/01"
     },
     {
         "title": "The cotton-jersey",
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
-        "image": "img/blog/b2.jpg",
+        "image": "/ecommerce/img/blog/female.png",
         "date": "13/01"
     },
     {
         "title": "The cotton-jersey",
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
-        "image": "img/blog/b3.jpg",
+        "image": "/ecommerce/img/blog/indigenous.png",
         "date": "13/01"
     },
     {
         "title": "The cotton-jersey",
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
-        "image": "img/blog/b4.jpg",
+        "image": "/ecommerce/img/blog/landscape.png",
         "date": "13/01"
     },
     {
         "title": "The cotton-jersey",
         "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
-        "image": "img/blog/b6.jpg",
+        "image": "/ecommerce/img/blog/modern.png",
         "date": "13/01"
     }
 ]
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,8 +1,10 @@
 import Blog from "../../components/Blog/Blog";
+import Header from "../../components/Header/Header";
 
 function BlogPage() {
     return (
         <>
+            <Header />
             <Blog />
         </>
     );
