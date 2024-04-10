c7f3b53a1c5f5e84115ed2050c652a793276aa66
dev-celo <marcelohenrique.backend@gmail.com>
2024-04-10 10:54:41 -0300

    Feat:Json dos posts criado.

---
+++ src/components/data/postsData.js
@@ -0,0 +1,32 @@
+export const postsData = [
+    {
+        "title": "The cotton-jersey",
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
+        "image": "img/blog/b1.jpg",
+        "date": "13/01"
+    },
+    {
+        "title": "The cotton-jersey",
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
+        "image": "img/blog/b2.jpg",
+        "date": "13/01"
+    },
+    {
+        "title": "The cotton-jersey",
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
+        "image": "img/blog/b3.jpg",
+        "date": "13/01"
+    },
+    {
+        "title": "The cotton-jersey",
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
+        "image": "img/blog/b4.jpg",
+        "date": "13/01"
+    },
+    {
+        "title": "The cotton-jersey",
+        "description": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus accusamus amet officiis obcaecati velit inventore! Earum aliquid culpa unde labore, vel illo, sed quaerat, veritatis porro aliquam nam vitae. Nobis.",
+        "image": "img/blog/b6.jpg",
+        "date": "13/01"
+    }
+]
