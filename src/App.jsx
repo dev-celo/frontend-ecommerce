1197d84b68ed40a26f4a121f74e2b4b33b11d182
dev-celo <marcelohenrique.backend@gmail.com>
2024-04-10 10:54:14 -0300

    feat: Implementando rota blog.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import DetailsProduct from './pages/DetailsProduct/DetailsProduct';
 import './app.css';
 import PageLogin from './pages/Login/PageLogin';
 import PageRegister from './pages/PageRegister/PageRegister';
+import BlogPage from './pages/Blog/Blog';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/ecommerce" Component={Home} />
         <Route path="/ecommerce/login" Component={PageLogin} />
         <Route path="/ecommerce/register" Component={PageRegister} />
+        <Route path="/ecommerce/blog" Component={BlogPage} />
         <Route path="/ecommerce/product/:typeProduct/:id" Component={DetailsProduct} />
       </Routes>
     </ShoppingCartProvider>
