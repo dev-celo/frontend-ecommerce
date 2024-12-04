367e0fca096431bad57aedd1ef6ceb2328162898
dev-celo <marcelohenrique.backend@gmail.com>
2024-12-04 17:17:06 -0300

    Updt: Modificando estilos do site

--- src/components/DoubleBanner/DoubleBanner.jsx
+++ src/components/DoubleBanner/DoubleBanner.jsx
@@ -4,15 +4,15 @@ export function DoubleBanner() {
   return (
     <section id="double-banner" className="section-p1" data-testid="double-banner">
       <div className="banner-box" data-testid="banner-box-1" id="banner-box-1">
-        <h4 data-testid="banner-title-1">Craze deals</h4>
-        <h2 data-testid="banner-subtitle-1">Buy 1 get 1 free</h2>
-        <span data-testid="banner-description-1">The best classNameic dress in on sale at cara</span>
+        <h4 data-testid="banner-title-1" className="background-text">Craze deals</h4>
+        <h2 data-testid="banner-subtitle-1" className="background-text">Buy 1 get 1 free</h2>
+        <span data-testid="banner-description-1" className="background-text">The best classNameic dress in on sale at cara</span>
         <button className="white" data-testid="banner-button-1">Learn More</button>
       </div>
       <div className="banner-box banner-box2" data-testid="banner-box-2" id="banner-box-2">
         <h4 data-testid="banner-title-2" className="background-text">spring/summer</h4>
         <h2 data-testid="banner-subtitle-2" className="background-text">Upcomming season</h2>
-        <p className="background-text" data-testid="banner-description-2">The best dress in on sale at cara</p>
+        <span data-testid="banner-description-2" className="background-text">The best dress in on sale at cara</span>
         <button className="white" data-testid="banner-button-2">Collection</button>
       </div>
     </section>
