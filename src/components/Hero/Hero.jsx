bbc277552e86ff164db58de37c024243689bea84
dev-celo <marcelohenrique.backend@gmail.com>
2025-01-21 09:35:02 -0300

    Fix: Resolvendo problema de responsividade

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <section id="hero" data-testid="hero" className="cont">
       <h4 id="trade-offer" data-testid="trade-offer">LEVE UM POUCO</h4>
-      <h2 id="super-deals" data-testid="super-deals"> Do INTERIOR CACAUREIRO BAIANO</h2>
+      <h2 id="super-deals" data-testid="super-deals"> DO INTERIOR CACAUREIRO BAIANO</h2>
       <h1 id="all-products" data-testid="all-products">PARA SUA CASA!</h1>
       <p id="coupons" data-testid="coupons">CHOCOLATE PRODUZIDO EM IPIAÚ-BA</p>
       <button id="shop-now" data-testid="shop-now">COMPRAR AGORA!</button>
