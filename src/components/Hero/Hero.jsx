fee3bb068ad0094f0f55aa0a46d2e11e750a5d98
dev-celo <marcelohenrique.backend@gmail.com>
2025-01-21 08:55:50 -0300

    Updt: atualizando a capa inicial dosite

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,11 +3,11 @@ import './hero.css';
 function Hero() {
   return (
     <section id="hero" data-testid="hero" className="cont">
-      <h4 id="trade-offer" data-testid="trade-offer">Trade-in-offer</h4>
-      <h2 id="super-deals" data-testid="super-deals">Super value deals</h2>
-      <h1 id="all-products" data-testid="all-products">On all products</h1>
-      <p id="coupons" data-testid="coupons">Save more with coupons & up to 70% off!</p>
-      <button id="shop-now" data-testid="shop-now">Shop Now</button>
+      <h4 id="trade-offer" data-testid="trade-offer">LEVE UM POUCO</h4>
+      <h2 id="super-deals" data-testid="super-deals"> Do INTERIOR CACAUREIRO BAIANO</h2>
+      <h1 id="all-products" data-testid="all-products">PARA SUA CASA!</h1>
+      <p id="coupons" data-testid="coupons">CHOCOLATE PRODUZIDO EM IPIAÚ-BA</p>
+      <button id="shop-now" data-testid="shop-now">COMPRAR AGORA!</button>
     </section>
   );
 }
